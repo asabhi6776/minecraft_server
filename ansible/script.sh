ba7652c67d3ae68a776a768229b46fa8e23b2ce7
Abhishek Singh <abhishek.abvp.as@gmail.com>
2022-11-07 14:52:43 +0530

    modifying script.sh

--- ansible/script.sh
+++ ansible/script.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-chmod +x /usr/local/bin/docker-compose
+install_docker_compose() {
+    apt install docker docker.io -y
+    curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+}
+
+
+which docker-compose
+
+if [[ $? -ne 0 ]]; then
+    install_docker_compose
+else
+    echo "going to start the container."
+
 
 
 cd /opt/repos
