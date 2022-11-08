c9e42dbabcf8a8b7d62c246b6f0b7f80b788ce9e
Abhishek Singh <abhishek.abvp.as@gmail.com>
2022-11-08 13:46:29 +0530

    ansible is ready

--- ansible/script.sh
+++ ansible/script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 install_docker_compose() {
+    apt update
     apt install docker docker.io -y
     curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
@@ -9,12 +10,16 @@ install_docker_compose() {
 
 which docker-compose
 
-if [[ $? -ne 0 ]]; then
+if [[ $? -ne 0 ]] ; then
     install_docker_compose
 else
     echo "going to start the container."
+fi
 
 
 
 cd /opt/repos
+docker-compose up -d
+chmod -R 777 data 
+docker-compose down
 docker-compose up -d
